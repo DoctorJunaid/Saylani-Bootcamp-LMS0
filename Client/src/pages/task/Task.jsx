c0016cfaaa28be4f24486a50df0b924dc32116e0
faizurrehman39804-eng <faizurrehman39804@gmail.com>
2026-08-10 22:14:52 +0500

    Add task table and update task components

--- Client/src/pages/task/Task.jsx
+++ Client/src/pages/task/Task.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import TaskHeader from "../../components/taskComponents/TaskHeader";
 import TaskStats from "../../components/taskComponents/TaskStats";
+import TaskTable from "../../components/taskComponents/TaskTable";
 
 const Task = () => {
   return (
-    <div className="flex flex-col gap-lg bg-[var(--color-background)] min-h-screen p-md sm:p-lg">
-      <div className="bg-[var(--color-surface)] border border-[var(--color-border)] rounded-[var(--radius-lg)] p-lg shadow-[var(--shadow-sm)]  ">
-        <h1>Tasks & Assignments</h1>
-        <TaskHeader />
-        <TaskStats />
+    <div className="flex flex-col gap-6 p-4 sm:p-6 bg-[var(--color-background)] min-h-screen">
+      {/* Header */}
+      <TaskHeader />
+
+      {/* Stats Cards */}
+      <TaskStats />
+
+      {/* Task List / Table Section (Inside reference bounding box) */}
+      <div className="bg-[var(--color-surface)] border border-[var(--color-border)] rounded-[var(--radius-lg)] p-4 sm:p-6 shadow-[var(--shadow-sm)]">
+        <TaskTable />
       </div>
     </div>
   );
 };
 
 export default Task;
+
